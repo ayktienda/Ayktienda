f4f843b5b2cc88161f5c0bb917b25dacd3f832aa
ayktienda <aykpersonalizados@gmail.com>
2026-09-07 17:14:17 -0300

    Create productos.js

---
+++ .github/productos.js
@@ -0,0 +1,8 @@
+const products = [
+  {id:1,name:'Mug personalizado',cat:'Personalizados',price:12990,img:'tazon_empresa.jpg',outlet:false},
+  {id:2,name:'Vaso de vidrio personalizado',cat:'Personalizados',price:7990,img:'jarro_spiderman.jpg',outlet:false},
+  {id:3,name:'Morral personalizado',cat:'Personalizados',price:2500,img:'bolsa_sonic.jpg',outlet:false},
+  {id:4,name:'Tazón personalizado',cat:'Personalizados',price:3990,img:'tazon_empresa.jpg',outlet:false},
+  {id:5,name:'Llavero personalizado NFC',cat:'Personalizados',price:5990,img:'tazon_empresa.jpg',outlet:false},
+  {id:6,name:'Mousepad personalizado',cat:'Personalizados',price:3500,img:'tazon_empresa.jpg',outlet:false}
+];
