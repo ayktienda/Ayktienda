df945e08bd5c7f7f0177024e4066b5c0414c677c
ayktienda <aykpersonalizados@gmail.com>
2026-09-08 11:08:22 -0300

    Update productos.js

--- productos.js
+++ productos.js
@@ -5,4 +5,4 @@ const products = [
   {id:4,name:'Tazón personalizado',cat:'Personalizados',price:3990,img:'tazon_empresa.jpg',outlet:false},
   {id:5,name:'Llavero personalizado NFC',cat:'Personalizados',price:5990,img:'tazon_empresa.jpg',outlet:false},
   {id:6,name:'Mousepad personalizado',cat:'Personalizados',price:3490,img:'tazon_empresa.jpg',outlet:false}
-];
+{id:5,name:'Tazón glitter',cat:'Personalizados',price:7490,img:'PENDIENTE.jpg',outlet:false},
