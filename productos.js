f59d23996683cbfaa2a851459b911e8a72c27174
ayktienda <aykpersonalizados@gmail.com>
2026-09-08 11:27:01 -0300

    Update productos.js

--- productos.js
+++ productos.js
@@ -4,5 +4,16 @@ const products = [
   {id:3,name:'Morral personalizado',cat:'Personalizados',price:2490,img:'bolsa_sonic.jpg',outlet:false},
   {id:4,name:'Tazón personalizado',cat:'Personalizados',price:3990,img:'tazon_empresa.jpg',outlet:false},
   {id:5,name:'Llavero personalizado NFC',cat:'Personalizados',price:5990,img:'tazon_empresa.jpg',outlet:false},
-  {id:6,name:'Mousepad personalizado',cat:'Personalizados',price:3490,img:'tazon_empresa.jpg',outlet:false}
-{id:5,name:'Tazón glitter',cat:'Personalizados',price:7490,img:'PENDIENTE.jpg',outlet:false},
+ {id:6,name:'Mousepad personalizado',cat:'Personalizados',price:3490,img:'tazon_empresa.jpg',outlet:false},
+{id:7,name:'Tazón glitter',cat:'Personalizados',price:7490,img:'PENDIENTE.jpg',outlet:false},
+{id:8,name:'Bolsa TNT 20x30',cat:'Personalizados',price:1200,img:'PENDIENTE.jpg',outlet:false},
+{id:9,name:'Bolsa TNT 27x33',cat:'Personalizados',price:1500,img:'PENDIENTE.jpg',outlet:false},
+{id:10,name:'Chapita alfiler',cat:'Personalizados',price:990,img:'PENDIENTE.jpg',outlet:false},
+{id:11,name:'Bolsa TNT 32x38',cat:'Personalizados',price:2000,img:'PENDIENTE.jpg',outlet:false},
+{id:12,name:'Chapita imán destapador',cat:'Personalizados',price:1900,img:'PENDIENTE.jpg',outlet:false},
+{id:13,name:'Chapita llavero destapador',cat:'Personalizados',price:1900,img:'PENDIENTE.jpg',outlet:false},
+{id:14,name:'Estuche de lino 15x24 cm',cat:'Personalizados',price:4990,img:'PENDIENTE.jpg',outlet:false},
+{id:15,name:'Estuche de lino 10x24 cm',cat:'Personalizados',price:3500,img:'PENDIENTE.jpg',outlet:false},
+{id:16,name:'Shopper',cat:'Personalizados',price:7500,img:'PENDIENTE.jpg',outlet:false},
+{id:17,name:'Botella de aluminio',cat:'Personalizados',price:7500,img:'PENDIENTE.jpg',outlet:false}
+];
